001359a0b1a9a5ee88584abbabd8405d67ffd000
mdinkelbach <mj.dinkelbach@mgail.com>
2022-11-02 11:23:15 -0500

    java 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
-let timer = document.getElementById("timer");
-let button = document.querySelector("button");
+let timer = document.getElementById('timer');
+let button = document.querySelector('button');
+let title = document.querySelector('.title');
 
-let timeLeft = 0
+let timeLeft = 0;
 
 function countdown(){
     timeLeft--;
@@ -11,7 +12,14 @@ function countdown(){
     }
 };
 
+function quiz() {
+    title.textContent = 'Question 1';
+}
+
 button.addEventListener('click', function() {
     timeLeft = 90
     setTimeout(countdown, 1000);
-})
+    quiz();
+});
+
+
