f84bffb80cb27e2ee4681416c4686022ec73f9d1
mdinkelbach <mj.dinkelbach@mgail.com>
2022-11-04 16:44:59 -0500

    js comments

--- assets/js/score-script.js
+++ assets/js/score-script.js
@@ -1,12 +1,15 @@
+// Variable definitions for HTML elements
 let newScore = document.createElement('li');
 let section = document.querySelector('section');
 let clearScore = document.getElementById('clear-score-button');
 
+// Loop to set score (could possibly be used to set multiple scores, but not needed for this challenge)
 for (let i = 0; i < localStorage.getItem("score").length; i++) {
     section.children[1].appendChild(newScore);
     section.children[1].children[0].textContent = `${localStorage.getItem("initials")} - ${localStorage.getItem("score")}`
 };
 
+// Clears scores when "Clear Score" button is pressed
 clearScore.addEventListener('click', function() {
     localStorage.removeItem("score");
     localStorage.removeItem("initials");
